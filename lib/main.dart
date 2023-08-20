1c404ba6ab50857968ce10fbb7e94395d09d5f63
jaewonE <importjaewonE@gmail.com>
2023-08-20 13:20:50 +0900

    Split detect cry service

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hear_ease_app/services/detect_cry.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:tflite_flutter/tflite_flutter.dart' as tfl;
-import 'package:flutter/services.dart';
-import 'package:wav/wav.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,21 +27,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool _recording = false;
-  static const String modelPath =
-      'assets/models/lite-model_yamnet_tflite_1.tflite';
-  static const String csvPath = 'assets/models/yamnet_class_map.csv';
+  bool _hasLoadModel = false;
+  late DetectCryService detectCry;
 
   @override
   void initState() {
     super.initState();
+    detectCry = DetectCryService(
+        modelPath: 'assets/models/lite-model_yamnet_tflite_1.tflite',
+        csvPath: 'assets/models/yamnet_class_map.csv');
+
     _recorder();
-  }
-
-  Future<List<double>> readWav(String filePath) async {
-    final wav = await Wav.readFile(filePath);
-    var mono = wav.toMono();
-
-    return mono;
   }
 
   void toggleRecord() {
@@ -53,75 +47,19 @@ class _MyHomePageState extends State<MyHomePage> {
     _recording ? _recorder() : _stop();
   }
 
-  Future<List<String>> getTamNetClasses() async {
-    String fileContent = await rootBundle.loadString(csvPath);
-
-    List<String> lines = fileContent.split('\n');
-
-    // 첫 번째 줄 (헤더)를 제거
-    lines.removeAt(0);
-
-    // 빈 줄 제거
-    lines.removeWhere((line) => line.trim().isEmpty);
-
-    // display_name만 추출
-    List<String> displayNames = lines.map((line) {
-      List<String> columns = line.split(',');
-      String displayName = columns[2];
-
-      // 쌍따옴표가 포함된 경우 제거
-      return displayName.replaceAll('"', '').trim();
-    }).toList();
-
-    return displayNames;
-  }
-
-  Map<String, double> getNameFromScore(
-      List<String> classes, List<double> prediction, int n) {
-    var indexedPrediction = prediction.asMap().entries.toList();
-
-    indexedPrediction.sort((a, b) => b.value.compareTo(a.value));
-
-    Map<String, double> sortedMap = Map.fromEntries(indexedPrediction
-        .take(n)
-        .map((entry) => MapEntry(classes[entry.key], entry.value)));
-
-    return sortedMap;
-  }
-
-  bool isBabyCry(Map<String, double> predictMap) {
-    List<String> babyClasses = ['Crying', 'Baby cry', 'Babbling'];
-    var targetList = predictMap.keys.toList();
-
-    for (String target in targetList) {
-      if (babyClasses.contains(target)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
   void _recorder() async {
     print("onPressed start");
+
+    if (_hasLoadModel == false) {
+      await detectCry.loadModelAsset();
+      _hasLoadModel = true;
+    }
+
     var dir = (await getApplicationDocumentsDirectory()).path;
     var filePath = '$dir/hungry_8.wav';
 
-    var input = await readWav(filePath);
-
-    var output = List.filled(1, List.filled(521, 0.0));
-
-    final interpreter = await tfl.Interpreter.fromAsset(modelPath);
-
-    // inference
-    interpreter.run(input, output);
-
-    // print the output
-    var classes = await getTamNetClasses();
-    var predictMap = getNameFromScore(classes, output[0], 5);
-    predictMap.forEach((key, value) {
-      print('$key: ${value.toStringAsFixed(2)}');
-    });
-    print(isBabyCry(predictMap));
+    bool isBabyCry = await detectCry.isBabyCry(filePath);
+    print('isBabyCry: $isBabyCry');
   }
 
   void _stop() async {
---
+++ lib/services/detect_cry.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:tflite_flutter/tflite_flutter.dart' as tfl;
+import 'package:flutter/services.dart';
+import 'package:wav/wav.dart';
+
+class DetectCryService {
+  late String modelPath = '';
+  late String csvPath = '';
+  late tfl.Interpreter interpreter;
+  static const List<String> babyClasses = ['Crying', 'Baby cry', 'Babbling'];
+
+  DetectCryService({required this.modelPath, required this.csvPath});
+
+  Future<void> loadModelAsset() async {
+    interpreter = await tfl.Interpreter.fromAsset(modelPath);
+  }
+
+  Future<bool> isBabyCry(String filePath) async {
+    var input = await readWav(filePath);
+    var output = List.filled(1, List.filled(521, 0.0));
+
+    // final interpreter = await tfl.Interpreter.fromAsset(modelPath);
+
+    // inference
+    interpreter.run(input, output);
+
+    // print the output
+    var classes = await getYamNetClasses();
+    var predictMap = _getNameFromScore(classes, output[0], 5);
+    printScoreMap(predictMap);
+    return defineCryByPrediction(predictMap);
+  }
+
+  Future<List<double>> readWav(String filePath) async {
+    final wav = await Wav.readFile(filePath);
+    var mono = wav.toMono();
+
+    return mono;
+  }
+
+  Future<List<String>> getYamNetClasses() async {
+    String fileContent = await rootBundle.loadString(csvPath);
+
+    List<String> lines = fileContent.split('\n');
+
+    // 첫 번째 줄 (헤더)를 제거
+    lines.removeAt(0);
+
+    // 빈 줄 제거
+    lines.removeWhere((line) => line.trim().isEmpty);
+
+    // display_name만 추출
+    List<String> displayNames = lines.map((line) {
+      List<String> columns = line.split(',');
+      String displayName = columns[2];
+
+      // 쌍따옴표가 포함된 경우 제거
+      return displayName.replaceAll('"', '').trim();
+    }).toList();
+
+    return displayNames;
+  }
+
+  bool defineCryByPrediction(Map<String, double> predictMap) {
+    var targetList = predictMap.keys.toList();
+
+    for (String target in targetList) {
+      if (babyClasses.contains(target)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  Map<String, double> _getNameFromScore(
+      List<String> classes, List<double> prediction, int n) {
+    var indexedPrediction = prediction.asMap().entries.toList();
+
+    indexedPrediction.sort((a, b) => b.value.compareTo(a.value));
+
+    Map<String, double> sortedMap = Map.fromEntries(indexedPrediction
+        .take(n)
+        .map((entry) => MapEntry(classes[entry.key], entry.value)));
+
+    return sortedMap;
+  }
+
+  void printScoreMap(Map<String, double> predictMap) {
+    predictMap.forEach((key, value) {
+      debugPrint('$key: ${value.toStringAsFixed(2)}');
+    });
+  }
+}
